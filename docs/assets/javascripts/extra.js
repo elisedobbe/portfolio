8f1551e39db16c3f6774aa74cd77eadd68a5c47e
elisedobbe <elisenettside@gmail.com>
2026-02-16 17:00:08 +0100

    Update extra.js

--- docs/assets/javascripts/extra.js
+++ docs/assets/javascripts/extra.js
@@ -1,8 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var title = document.querySelector(".md-header__title");
+  var title = document.querySelector(".md-header__inner .md-header__title");
+  if (!title) {
+    title = document.querySelector(".md-header__title");
+  }
+  if (!title) {
+    title = document.querySelector(".md-header__topic .md-ellipsis");
+  }
   if (title) {
     title.style.cursor = "pointer";
-    title.addEventListener("click", function() {
+    var parent = title.closest(".md-header__title") || title;
+    parent.style.cursor = "pointer";
+    parent.addEventListener("click", function() {
       window.location.href = "/portfolio/";
     });
   }
